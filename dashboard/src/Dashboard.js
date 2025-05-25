1c7e66a356a3db0a84f1a5d4bbddfeb51e83d2f2
Ilyasse Boudjemline <ilyasseboudjemline@gmail.com>
2025-05-25 10:55:01 +0100

    Change PNG to SVG

--- dashboard/src/Dashboard.js
+++ dashboard/src/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Dashboard.css';
-import logo from './assets/logo.png';
+import logo from './assets/logo.svg';
 import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
